ba69c0d24c9082e05875b3a15e4afb70e2cf6e72
Jeff Comstock <jeff@ologicinc.com>
2018-10-08 11:19:18 -0700

    Added main.c comments to describe project periphs, added USB Serial COM support periph

--- SamD21G18A/Frosted_Flakes/Frosted_Flakes/main.c
+++ SamD21G18A/Frosted_Flakes/Frosted_Flakes/main.c
@@ -1,3 +1,19 @@
+/*
+	Frosted Flakes:
+	A mainly plain cereal with a bit of added sugar. This is a test program and meant to
+	offer solid foundation for the challenges to come. Normally I would say corn flakes
+	are my go to but in this case we are using the higher level initializations of Atmel Start.
+	When we chose to go a more bare metal approach in the time to come you can find me eating some
+	corn flakes. For now, I will rely on the added sugar.
+	
+	Peripherals:
+	VidorSPI - Sercom3
+				MISO PA19
+				MOSI PA16
+				SCK  PA17
+				CS - N/A <-- Haven't picked yet
+*/
+
 #include <atmel_start.h>
 
 int main(void)
