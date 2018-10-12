deda9b810f85be85f0c4a0ec6b536a3da0435740
Jeff Comstock <jeff@ologicinc.com>
2018-10-11 23:25:20 -0700

    created testbench for counter and debuged counter code. Should increment now

--- cyclone10/projects/MKRVIDOR4000_template/Counter.v
+++ cyclone10/projects/MKRVIDOR4000_template/Counter.v
@@ -9,19 +9,71 @@ module Counter
 //reg rCounter[$bits(pCompareVal) - 1 : 0] = 0;
 reg [31:0] rCounter = 32'b0;
 
-always @(posedge (wClk), posedge(wRst)) begin
-	if (wRst == 1'b1)
+always @(posedge (wClk) or posedge(wRst)) begin
+	if (wRst)
 	begin
-		rCounter[31:0] <= 32'b0;
-		rOutCount[7:0]	<= 8'b11111111;
+		rCounter 	<= 0;
+		rOutCount	<= 8'b11111111;
 	end
 	else
 	begin
-		if (rCounter[31:0] & 32'b11110000111100001111000011110000)
-		begin
-			rOutCount[7:0] <= rOutCount[7:0] + 1;
-		end
+		//if (rCounter[31:0] & 32'b11110000111100001111000011110000) //not sure what this means, it will never count with this if statement
+		//begin
+			rOutCount <= rOutCount + 1;
+		//end
 	end
 end
 
+endmodule
+
+
+
+module counter_tb;
+
+	reg wClk, wRst;
+	wire [7:0] rOutCount;
+	
+	parameter delay = 5;
+	
+Counter DUT(wClk, wRst, rOutCount);
+
+initial
+begin
+
+wClk = 0; wRst = 0;
+#50 wRst = 1;
+#delay wRst = 0;
+#200;
+end
+
+always
+begin
+#delay wClk = 1;
+#delay wClk = 0;
+#delay wClk = 1;
+#delay wClk = 0;
+#delay wClk = 1;
+#delay wClk = 0;
+#delay wClk = 1;
+#delay wClk = 0;
+#delay wClk = 1;
+#delay wClk = 0;
+#delay wClk = 1;
+#delay wClk = 0;
+#delay wClk = 1;
+#delay wClk = 0;
+#delay wClk = 1;
+#delay wClk = 0;
+#delay wClk = 1;
+#delay wClk = 0;
+#delay wClk = 1;
+#delay wClk = 0;
+#delay wClk = 1;
+#delay wClk = 0;
+#delay wClk = 1;
+#delay wClk = 0;
+
+end
+ 
+	
 endmodule
